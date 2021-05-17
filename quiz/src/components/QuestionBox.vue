ba80ae16df1ab23f5a299492fc1c207c7360b047
BrianH <brianhwang29@gmail.com>
2021-05-17 12:56:17 +0900

    update select

--- quiz/src/components/QuestionBox.vue
+++ quiz/src/components/QuestionBox.vue
@@ -6,10 +6,17 @@
       </template>
 
       <hr class="my-4">
-
-      <p v-for="(answer,i) in answers" :key="i">
-        {{answer}}
-      </p>
+      <b-list-group>
+        <b-list-group-item 
+          v-for="(answer,i) in answers" 
+          :key="i" 
+          @click="selectAnswer(i)" 
+          :class="[selectedIndex === i ? 'selected' : '']"
+          
+          >
+          {{answer}}
+        </b-list-group-item>
+      </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button @click="next" variant="success" href="#">Next</b-button>
@@ -18,10 +25,24 @@
 </template>
 
 <script>
+import _ from 'lodash'
+
 export default({
  props:{
    currentQuestion: Object,
    next: Function
+ },
+ data(){
+   return{
+     selectedIndex: null,
+     shuffledAnswers: []
+   }
+ },
+ watch: {
+   currentQuestion(){
+     this.selectedIndex = null
+     this.shuffleAnswers()
+   }
  },
  computed: {
    answers(){
@@ -30,8 +51,41 @@ export default({
      return answers
    }
  },
- mounted(){
-   console.log(this.currentQuestion); 
+ methods:{
+   selectAnswer(i) {
+     this.selectedIndex = i
+   },
+   shuffleAnswers(){
+     let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+     this.shuffledAnswers = _.shuffle(answers)
+   }
  }
 })
 </script>
+
+<style scoped>
+  .list-group{
+    margin-bottom: 15px; 
+  }
+
+  .btn{
+    margin: 0 5px
+  }
+
+  .list-group-item:hover{
+    background: #eee;
+    cursor: pointer; 
+  }
+
+  .selected{
+    background-color: lightblue; ;
+  }
+
+  .correct {
+    background-color: green;
+  }
+
+  .inCorrect {
+    background-color: red;
+  }
+</style>
