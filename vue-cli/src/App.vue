d4599cba238f9b043d0d085234e09d5fefb5b381
BrianH <brianhwang29@gmail.com>
2021-07-19 15:59:03 +0900

    vue cli importing component

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    {{ str }}
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
+
+//basic vue component 입력 방식 
+// var AppHeader = {
+//   template: '<header><h1>Header</h1></header>'
+// }
+
+//basic vue instance 입력 방식 
 // new Vue({
 //   data:{
 //     str: 'hi'
@@ -16,6 +24,9 @@ export default {
     return {
       str: 'hi'
     }
+  },
+  components: {
+    'app-header': AppHeader
   }
 }
 </script>
