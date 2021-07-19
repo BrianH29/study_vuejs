c346be7efa4657b97c0fcfb579c12efce9d18d19
BrianH <brianhwang29@gmail.com>
2021-07-19 15:41:08 +0900

    initial vue cli:

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ str }}
+  </div>
+</template>
+
+<script>
+// new Vue({
+//   data:{
+//     str: 'hi'
+//   }
+// });
+
+export default {
+  data:function(){
+    return {
+      str: 'hi'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,28 @@
+// var appHeader = {
+//   template : '<div>Header</div>',
+//   methods: {
+//     addNumber : function(){
+      
+//     }
+//   }
+// }
+
+<template>
+  <!--html-->
+  <div>Header</div>
+</template>
+
+<script>
+export default {
+  //javascript
+  methods: {
+    addNumber : function(){
+      
+    }
+  }
+}
+</script>
+
+<style>
+  /* css */
+</style>
